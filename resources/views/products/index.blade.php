b0ded900b26c2c7888bedb91ac50fe45839f88e8
oscar caceres <lightcaceres@gmail.com>
2022-04-02 16:46:27 -0600

    products view

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,4 +1,4 @@
-<ul>
+{{-- <ul>
     @foreach ($products as $product)
     <li>{{$product}}</li>   
    @endforeach
@@ -7,4 +7,35 @@
     @foreach ($proveedores as $proveedor)
     <li>{{$proveedor}}</li>   
    @endforeach
-</ul>
+</ul> --}}
+
+
+@extends('layouts.app')
+
+@section('css')
+
+@endsection
+
+@section('content')
+
+<h2 class="text-center mb-5">Administración de productos</h2>
+<div class="col-md-10 mx-auto bg-white p-3">
+<table class="table">
+    <thead class="bg-danger text-light">
+        <tr>
+            <th scole="col">Codigo</th>
+            <th scole="col">Nombre</th>
+            <th scole="col">Acciones</th>
+        </tr>
+
+    </thead>
+</table>
+</div>
+
+
+
+@endsection
+
+@section('js')
+
+@endsection
